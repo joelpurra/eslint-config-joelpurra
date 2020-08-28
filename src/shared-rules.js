198818b0c627acac3eeaa0ae8697010b1156ec25
Joel Purra <mig@joelpurra.se>
2020-08-28 14:10:00 +0200

    Add xo and rules

---
+++ src/shared-rules.js
@@ -0,0 +1,74 @@
+const sharedRules = () => ({
+	"array-bracket-newline": [
+		"error",
+		{
+			minItems: 1,
+			multiline: true,
+		},
+	],
+	"array-bracket-spacing": [
+		"error",
+		"always",
+	],
+	"array-element-newline": [
+		"error",
+		"always",
+	],
+	"arrow-parens": [
+		"error",
+		"always",
+	],
+	"capitalized-comments": "off",
+	"comma-dangle": [
+		"error",
+		"always-multiline",
+	],
+	"function-call-argument-newline": [
+		"error",
+		"consistent",
+	],
+	"no-console": "error",
+	"no-sync": "error",
+	"no-tabs": [
+		"error",
+		{
+			allowIndentationTabs: true,
+		},
+	],
+	"no-unused-vars": "warn",
+	"no-useless-concat": "error",
+	"no-warning-comments": "off",
+	"object-curly-newline": [
+		"error",
+		{
+			minProperties: 1,
+		},
+	],
+	"object-curly-spacing": [
+		"error",
+		"always",
+	],
+	"object-property-newline": "error",
+	"operator-linebreak": [
+		"error",
+		"before",
+	],
+	"promise/always-return": "error",
+	"promise/catch-or-return": "error",
+	"quote-props": [
+		"error",
+		"as-needed",
+		{
+			keywords: true,
+		},
+	],
+	quotes: [
+		"error",
+		"double",
+	],
+	"simple-import-sort/sort": "error",
+	"sort-keys": "error",
+	"spaced-comment": "off",
+});
+
+module.exports = sharedRules;
